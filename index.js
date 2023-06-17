b8d485d1602e54e01f9659bc392d004ba7b7fd8d
omardieh <omar.dieh@wildcodeschool.com>
2023-06-17 20:36:46 +0200

    express app with response.SendFile() method

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const myColors = require("colors/safe");
+const express = require("express");
+const app = express();
 
-console.log(myColors.yellow("hello")); // outputs green text
-console.log(myColors.red.underline("i like cake and pies")); // outputs red underlined text
-console.log(myColors.inverse("inverse the color")); // inverses the color
-console.log(myColors.rainbow("OMG Rainbows!")); // outputs text in colors of a rainbow
-console.log(myColors.trap("Run the trap")); // Outputs the text using the trap fon
+app.use(express.static("public"));
+// app.get("/home", (request, response, next) => {
+//   // ...
+//   response.send(`<!doctype html>
+//   <html>
+//     <head>
+//       <meta charset="utf-8">
+//       <title>Cat</title>
+//       <link rel="stylesheet" href="/stylesheets/style.css" />
+//     </head>
+//     <body>
+//       <h1>Cat</h1>
+//       <p>This is my second route</p>
+//       <img src="/images/pic.png" />
+//     </body>
+//   </html>
+// `);
+// });
+
+// ...
+// our first Route:
+
+app.get("/home", (request, response, next) =>
+  response.sendFile(__dirname + "/views/home-page.html")
+);
+
+// cat route:
+app.get("/cat", (request, response, next) =>
+  response.sendFile(__dirname + "/views/cat-page.html")
+);
+// ...
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
